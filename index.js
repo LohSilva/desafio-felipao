42069c86613c13942c6634444add08d193f1fcce
LohSilva <lo-ruama_sp@hotmail.com>
2024-03-05 22:10:16 -0300

    Estrutura IF e ELSE

--- index.js
+++ index.js
@@ -3,4 +3,30 @@ const prompt = require('prompt-sync')();
 //Desafio Classificador de Nível de Herói
 //Declarando variável
 let nomeHeroi = prompt("Digite o nome do herói: ");
-let pontosExperienciaHeroi = prompt("Digite a quantidade de experiência do herói: ");
+let pontosXPHeroi= prompt("Digite a quantidade de experiência do herói: ");
+
+let nivel = "";
+
+
+//Verificando Nível do Herói
+
+if (pontosXPHeroi <= 1.000){
+    nivel = "Ferro";
+}else if (pontosXPHeroi <= 2.000){ 
+    nivel = "Bronze";
+}else if (pontosXPHeroi <= 5.000){
+    nivel = "Prata";
+}else if (pontosXPHeroi <= 7.000){
+    nivel = "Ouro";
+}else if (pontosXPHeroi <= 8.000){
+    nivel = "Platina";
+}else if (pontosXPHeroi <= 9.000){
+    nivel = "Ascendente";
+}else if (pontosXPHeroi <= 10.000){
+    nivel = "Imortal";
+}else{
+    nivel = "Radiante";
+}
+
+//Saída
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel);
