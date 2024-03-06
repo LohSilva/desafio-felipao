3bfde6dde8d58188d071ee80cfe2df976ab549cd
LohSilva <lo-ruama_sp@hotmail.com>
2024-03-05 22:28:33 -0300

    Incluíndo Operadores Lógicos

--- index.js
+++ index.js
@@ -9,20 +9,19 @@ let nivel = "";
 
 
 //Verificando Nível do Herói
-
 if (pontosXPHeroi <= 1.000){
     nivel = "Ferro";
-}else if (pontosXPHeroi <= 2.000){ 
+}else if (pontosXPHeroi >= 1.001 && pontosXPHeroi <= 2.000){ 
     nivel = "Bronze";
-}else if (pontosXPHeroi <= 5.000){
+}else if (pontosXPHeroi >= 2.001 && pontosXPHeroi <= 5.000){
     nivel = "Prata";
-}else if (pontosXPHeroi <= 7.000){
+}else if (pontosXPHeroi >= 5.001 && pontosXPHeroi <= 7.000){
     nivel = "Ouro";
-}else if (pontosXPHeroi <= 8.000){
+}else if (pontosXPHeroi >= 7.001 && pontosXPHeroi <= 8.000){
     nivel = "Platina";
-}else if (pontosXPHeroi <= 9.000){
+}else if (pontosXPHeroi >= 8.001 && pontosXPHeroi <= 9.000){
     nivel = "Ascendente";
-}else if (pontosXPHeroi <= 10.000){
+}else if (pontosXPHeroi >= 9.001 && pontosXPHeroi <= 10.000){
     nivel = "Imortal";
 }else{
     nivel = "Radiante";
